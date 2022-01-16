a78f8360e0626c38e9e07364e6d6e5cbd24e5244
Tanner Bell <coffmanfb45@gmail.com>
2022-01-16 14:08:32 -0500

    Fixed add_attendee

--- src/api/manager.py
+++ src/api/manager.py
@@ -17,6 +17,24 @@ def add_attendee():
     if 'rooms' in request.json:
         stmt = sqlalchemy.insert(attendee_rooms_table).values(
             room_id=request.json['rooms'], attendee_id=at.id)
+        db.session.execute(stmt)
         db.session.commit()
 
     return jsonify(at.serialize())
+
+
+@bp.route('/<int:id>', methods=['DELETE'])
+def delete_attendee(id: int):
+    """
+    Delete Attendee Type from records
+    """
+
+    try:
+        Attendee_Type.query.filter_by(id=id).delete()
+
+        db.session.commit()
+
+        return jsonify(True)
+
+    except:
+        return jsonify(False)
